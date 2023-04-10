6a0933a0b1a6642a2afe781fe20eb7c8b965ced0
Nadir Haciyev <h.nadir0903@gmail.com>
2023-04-10 18:16:50 +0400

    build: configuration

---
+++ src/pages/index.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import ReactPlayer from 'react-player'
+
+export default function Home() {
+  const [stream, setStream] = useState(null)
+
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined') {
+  //     navigator.mediaDevices
+  //       .getUserMedia({
+  //         audio: true,
+  //         video: true,
+  //         video: {
+  //           width: 1920,
+  //           height: 1080,
+  //           frameRate: { ideal: 60, max: 60 },
+  //           facingMode: 'user'
+  //         }
+  //       })
+  //       .then((stream) => setStream(stream));
+  //   }
+  // }, []);
+
+  return (
+    // <>{stream && <ReactPlayer className="!w-screen !h-screen" playing muted url={stream} />}</>
+    <div>ad</div>
+  )
+}
